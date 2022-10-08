a4d4506eee93c6fc3a02862e54905e6c7a86eb1e
Mihyar Al Masalma <eng.mihyear@gmail.com>
2022-10-08 02:04:44 -0300

    Add Dark/light mode

--- js/script.js
+++ js/script.js
@@ -30,6 +30,24 @@
         });
     });
 
+    $('.theme-switch').on('change', (e) => {
+        if (e.target.checked) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark'); //add this
+        } else {
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light'); //add this
+            $(".navbar-toggler-icon").toggleClass("no-background-image");
+        }
+    });
+
+    // Run once website load
+    const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
+        if (currentTheme === 'dark') toggleSwitch.checked = true;
+    };
     $("#home").trigger("click");
 
     console.info(`
